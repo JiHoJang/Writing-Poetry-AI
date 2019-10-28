29cf910c800f25556f557c35cfb9082476679b49
JiHoJang <45346783+JiHoJang@users.noreply.github.com>
2019-10-29 01:58:32 +0900

    Add files via upload

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -5,35 +5,42 @@ from langdetect import detect
 
 words = []
 
+num_poem = 0
+
 with open('unim_poem.json') as json_file:
     data = json.load(json_file)
     for p in data:
-        if detect(p['poem']) == 'en':
-            string = p['poem']
-            string = re.sub(r"[^A-Za-z0-9(),!?\'\`_]", " ", string)
-            string = re.sub(r"\'s", " \'s", string)
-            string = re.sub(r"\'ve", " \'ve", string)
-            string = re.sub(r"n\'t", " n\'t", string)
-            string = re.sub(r"\'re", " \'re", string)
-            string = re.sub(r"\'d", " \'d", string)
-            string = re.sub(r"\'ll", " \'ll", string)
-            string = re.sub(r",", " , ", string)
-            string = re.sub(r"!", " ! ", string)
-            string = re.sub(r"\(", " \( ", string)
-            string = re.sub(r"\)", " \) ", string)
-            string = re.sub(r"\?", " \? ", string)
-            string = re.sub(r"\s{2,}", " ", string)
-            string = re.sub("__LaTex__", "", string)
-            specialwords = ['EOS', 'GOO', '__LaTex__']
-            toLower = lambda x: " ".join( a if a in specialwords else a.lower() \
-                for a in x.split() )
-#            print(toLower(string.strip()))
-            words += toLower(string.strip()).split()
-            
+#        print(p['poem'].split('\n')[0].strip()))
+        temp = p['poem'].replace('_', ' ')
+        try:
+            if detect(temp) == 'en':
+                string = temp.replace('\"', '').replace('_', ' ').replace(',', ' , ').replace('!', ' ! ').replace(':', ' : ').lower()
+                string = re.sub(r"[^A-Za-z0-9(),!?\'\`_]", " ", string)
+                string = re.sub(r"\'s", " \'s", string)
+                string = re.sub(r"\'ve", " \'ve", string)
+                string = re.sub(r"n\'t", " n\'t", string)
+                string = re.sub(r"\'re", " \'re", string)
+                string = re.sub(r"\'d", " \'d", string)
+                string = re.sub(r"\'ll", " \'ll", string)
+                string = re.sub(r",", " , ", string)
+                string = re.sub(r"!", " ! ", string)
+                string = re.sub(r"\(", " \( ", string)
+                string = re.sub(r"\)", " \) ", string)
+                string = re.sub(r"\?", " \? ", string)
+                string = re.sub(r"\s{2,}", " ", string)
+
+                words += string.split()
+                num_poem += 1
+            else :
+                print('not english')
+        except Exception:
+            pass
+
+print('Complete unim_poem')  
 with open('gutenberg-poetry-v001.ndjson') as f:
     data = ndjson.load(f)
     for p in data:
-        string = p['s']
+        string = p['s'].replace('\"', '').replace('_', ' ').replace(',', ' , ').replace('!', ' ! ').replace(':', ' : ').lower()
         string = re.sub(r"[^A-Za-z0-9(),!?\'\`_]", " ", string)
         string = re.sub(r"\'s", " \'s", string)
         string = re.sub(r"\'ve", " \'ve", string)
@@ -47,20 +54,15 @@ with open('gutenberg-poetry-v001.ndjson') as f:
         string = re.sub(r"\)", " \) ", string)
         string = re.sub(r"\?", " \? ", string)
         string = re.sub(r"\s{2,}", " ", string)
-        string = re.sub("__LaTex__", "", string) # consider how to deal with latex later.
-        # prevent some special words from converting to lowercase.
-        specialwords = ['EOS', 'GOO', '__LaTex__']
-        toLower = lambda x: " ".join( a if a in specialwords else a.lower() \
-            for a in x.split() )
-        words += toLower(string.strip()).split()
 
+        words += string.split()
 words = list(set(words))
 
 print(len(words))
 
 
 
-f = open("output.txt", 'w')
+f = open("output2.txt", 'w')
 for i in words:
     f.write(i + ' ')
 f.close()
